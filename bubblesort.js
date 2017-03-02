e83351ce191d4f45b75d2ccfa049735ed39533c8
Chloe Choi <chloe.meowc@gmail.com>
2017-03-02 14:59:06 -0500

    initialized project

--- bubblesort.js
+++ bubblesort.js
@@ -1,5 +1,35 @@
-function bubbleSort(arr){
-    //hello
-    var output=[];
-    return output;
-}
+// function bubbleSort(arr){
+//
+//     var temp = 0;
+//     for (var i = 0; i < arr.length - 1; i++) {
+//         for(var z=i+1;z<arr.length; z++){
+//             if(arr[i] > arr[z]){
+//                 temp = arr[i];
+//                 console.log(arr[i]);
+//             }
+//         }
+//     }
+//     return output;
+// }
+
+
+
+// //
+// //     for (var j = i + 1; j < output.length; j++) {
+// //
+// //         // if(output[i] > output[j]){
+// //         //     temp = output[i];
+// //         //     output[i] = output[j];
+// //         //     output[j]=temp;
+// //         // }
+// //
+//
+//
+//         if (output[j-1] > output[j]) {
+//             temp = output[j - 1];
+//             output[j - 1] = output[j];
+//             output[j] = temp;
+//             // temp = output[j];
+//             console.log(output);
+//         }
+//     }
--- bubbluesort.spec.js
+++ bubbluesort.spec.js
@@ -2,4 +2,15 @@ describe('Bubble Sort', function(){
     it('handles an empty array', function(){
         expect( bubbleSort([]) ).toEqual( [] );
     });
+
+    it ('returns a perfectly sorted array', function(){
+        expect ( bubbleSort([5, 3, 1]) ). toEqual([1, 3, 5]);
+    });
+
+    it ('also cares the same numbers in different order', function() {
+        expect ( bubbleSort([8, 2, 1, 5, 3, 2, 7]) ). toEqual([1, 2, 2, 3, 5, 7, 8]);
+    });
+
+
 });
+
