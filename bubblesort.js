767bd156673d4bffc885043390142ee718e5f727
MarcosRS <MarcosRS16@gmail.com>
2017-03-02 15:04:13 -0500

    bublesort-completed

--- bubblesort.js
+++ bubblesort.js
@@ -1,35 +1,17 @@
-// function bubbleSort(arr){
-//
-//     var temp = 0;
-//     for (var i = 0; i < arr.length - 1; i++) {
-//         for(var z=i+1;z<arr.length; z++){
-//             if(arr[i] > arr[z]){
-//                 temp = arr[i];
-//                 console.log(arr[i]);
-//             }
-//         }
-//     }
-//     return output;
-// }
+function bubbleSort(arr){
 
+    var temp = 0;
+    for (var i = 0; i < arr.length - 1; i++) {
+        
+        for(var z=i+1;z < arr.length; z++){
+            
+            if(arr[i] > arr[z]){
+                temp = arr[i];
+            	arr[i] = arr[z];
+            	arr[z] = temp;    
+            }
+        }
 
-
-// //
-// //     for (var j = i + 1; j < output.length; j++) {
-// //
-// //         // if(output[i] > output[j]){
-// //         //     temp = output[i];
-// //         //     output[i] = output[j];
-// //         //     output[j]=temp;
-// //         // }
-// //
-//
-//
-//         if (output[j-1] > output[j]) {
-//             temp = output[j - 1];
-//             output[j - 1] = output[j];
-//             output[j] = temp;
-//             // temp = output[j];
-//             console.log(output);
-//         }
-//     }
+    }
+    return arr;
+}
